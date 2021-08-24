4d0ead591f7226d717529079fb1047b62f193900
Jacob Guiro <jguiro09@gmail.com>
2021-08-24 17:50:14 -0500

    React imported, Intro.js created/Intro.css created, Text added to Intro.js

---
+++ src/components/Intro.js
@@ -0,0 +1,16 @@
+import '../styles/Intro.css'
+
+export default function Intro() {
+    return(
+        <div className="container">
+            <div>
+                Hello. <br></br>
+                I'm <br></br>
+                Jacob Guiro
+            </div>
+            <div className = "subText">
+                Developer
+            </div>
+        </div>
+    )
+}
